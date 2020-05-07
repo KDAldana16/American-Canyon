81f1c3d06d566f071a819d904584f1926db3af3d
Karla Aldana <karla.aldana@students.dominican.edu>
2020-05-07 11:15:32 -0700

    Initial Commit

---
+++ American Canyon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  American Canyon
+//
+//  Created by Karla Aldana on 5/7/20.
+//  Copyright © 2020 Karla Aldana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
