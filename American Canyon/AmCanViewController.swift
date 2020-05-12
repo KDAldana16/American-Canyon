a10acdecaada9f9ce7621a58e6e806a7f9d44a7b
Karla Aldana <karla.aldana@students.dominican.edu>
2020-05-11 21:48:44 -0700

    commit

--- American Canyon/AmCanViewController.swift
+++ American Canyon/AmCanViewController.swift
@@ -17,7 +17,6 @@ class AmCanViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
